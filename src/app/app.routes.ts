83cdfcad5ddba00bd3cebc16a44878ff3d55168e
Alex Shtern <alexshtern1989@gmail.com>
2024-01-14 21:37:07 +0200

    added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '' },
+
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/components/users-table/users-table.component').then(
+        (m) => m.UsersTableComponent
+      ),
+  },
+  {
+    path: 'documents',
+    loadComponent: () =>
+      import(
+        './documents/components/documents-table/documents-table.component'
+      ).then((m) => m.DocumentsTableComponent),
+  },
+];
---
+++ src/app/documents/components/documents-table/documents-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documents-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './documents-table.component.html',
+  styleUrl: './documents-table.component.scss'
+})
+export class DocumentsTableComponent {
+
+}
