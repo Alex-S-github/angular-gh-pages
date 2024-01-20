34f16fd899e82d3fa7fe41e153d22d1ad4e54db8
Alex Shtern <alexshtern1989@gmail.com>
2024-01-20 18:49:42 +0200

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'gh-pages';
   // ng build --output-path docs --base-href /angular-gh-pages/
 
   navigate(): void {
     console.log('AppComponent ~ navigate ~ navigate:');
+  }
+
+  nav(link: string): void {
+    console.log('AppComponent ~ nav ~ link:', link);
+    this.router.navigate([link]);
   }
 }
